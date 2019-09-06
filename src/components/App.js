f3386a382d9d658a62146da9ecaae0a5f4624291
Garland Lai <garlandlai24@gmail.com>
2019-09-06 12:46:15 -0700

    trying to figure out the routing

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,19 @@ import Header from "./Header";
 import KegList from "./KegList";
 import NewKegForm from "./NewKegForm";
 import EditKegForm from "./EditKegForm";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    <NavButtons/>
-    <Header/>
-    <KegList/>
-    <NewKegForm/>
-    <br/>
-    <EditKegForm/>
-    </div>
+      <div className="App">
+        <NavButtons/>
+        <Header/>
+        <KegList/>
+        <BrowserRouter>
+        <Route path="/new" component={NewKegForm} />
+        <Route path="/edit" component={EditKegForm} />
+        </BrowserRouter>
+      </div>
   );
 }
 
