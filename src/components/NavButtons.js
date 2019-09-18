f47002e713f6089cc47d86a3856509d655b51166
Student <example@example.com>
2019-09-18 11:56:26 -0700

    add better NavBar

--- src/components/NavButtons.js
+++ src/components/NavButtons.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-import { Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom'
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import Header from './Header';
 
 
 function NavButtons(){
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
-        <Nav className="mr-auto">
-          <Link to="/">Home</Link>
-          <Link to="/about">About Us</Link>
-          <Link to="/products"> Product List</Link>
-          <Link to="/admin">Admin List</Link>
-          <Link to="/new"> Add New Flavors</Link>
-        </Nav>
-      </Navbar>
-      <Header/>
+    <Navbar bg="dark" variant="dark">
+    <Nav className="mr-auto">
+    <NavItem>
+    <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
+    </NavItem>
+    <NavLink to="/about" className="nav-link">About Us</NavLink>
+    <NavLink to="/products" className="nav-link"> Product List</NavLink>
+    <NavLink to="/admin" className="nav-link">Admin List</NavLink>
+    <NavLink to="/new" className="nav-link"> Add New Flavors</NavLink>
+    </Nav>
+    </Navbar>
+    <Header/>
     </div>
   );
 }
