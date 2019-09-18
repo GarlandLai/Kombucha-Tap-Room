69871db222e98169a9912ed46ea36303dd63be31
Student <example@example.com>
2019-09-18 12:09:08 -0700

    update Navbutton component a but

--- src/components/NavButtons.js
+++ src/components/NavButtons.js
@@ -9,9 +9,7 @@ function NavButtons(){
     <div>
     <Navbar bg="dark" variant="dark">
     <Nav className="mr-auto">
-    <NavItem>
     <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
-    </NavItem>
     <NavLink to="/about" className="nav-link">About Us</NavLink>
     <NavLink to="/products" className="nav-link"> Product List</NavLink>
     <NavLink to="/admin" className="nav-link">Admin List</NavLink>
