70b9d2e9815f8749032d715f9ff3a8cdba1bfa76
Student <example@example.com>
2019-09-19 10:03:20 -0700

    Using Hashrouter right now instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
